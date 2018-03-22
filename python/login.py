f7866e8e1ca054fb9f34cdeee9ba0cf5494b09be
Sivadass N <nsivadass@gmail.com>
2018-03-22 23:47:46 +0530

    Removed unused files

--- python/login.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-driver = webdriver.Firefox()
-driver.get('http://sivadass.in/')
-aboutButton = driver.find_elment_by_link_text("Know more about me!")
-aboutButton.click()

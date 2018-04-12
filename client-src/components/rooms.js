427a69096e42a8a7567641206c0740b36c79f33c
Sivadass N <nsivadass@gmail.com>
2018-04-13 00:30:36 +0530

    Fixed API typo

--- client-src/components/rooms.js
+++ client-src/components/rooms.js
@@ -23,7 +23,7 @@ class Rooms extends React.Component {
     let renderRooms = this.state.rooms.map(item => {
       return (
         <div className="room-item" key="item.id">
-          {item.bed} beds at {item.price}
+          {item.beds} beds at {item.price}
         </div>
       );
     });
